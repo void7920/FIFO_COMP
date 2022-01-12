d43cbda70bccee95a42fd8634369c3fc3b4d8a12
void7920 <w_pawn@outlook.com>
2022-01-12 23:09:09 +0900

    Create Tb_FIFO.v

---
+++ FIFO_COMP.srcs/sim_1/new/Tb_FIFO.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2022/01/12 20:28:28
+// Design Name: 
+// Module Name: Tb_FIFO
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Tb_FIFO(
+
+    );
+endmodule
